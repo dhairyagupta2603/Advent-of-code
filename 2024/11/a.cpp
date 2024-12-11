3ae8ec9456ce02688d7672c99ec246cb9563ae74
dhairyagupta2603 <72385022+dhairyagupta2603@users.noreply.github.com>
2024-12-11 15:01:51 +0530

    day11-1

---
+++ 2024/11/a.cpp
@@ -0,0 +1,77 @@
+#include <algorithm>
+#include <cassert>
+#include <chrono>
+#include <cmath>
+#include <fstream>
+#include <iostream>
+#include <list>
+
+auto read(std::fstream& f) {
+    std::list<long long> stones;
+    long long num = -1;
+    while (f >> num) {
+        stones.emplace_back(num);
+    }
+
+    return stones;
+}
+
+void print(const std::list<long long>& stones) {
+    for (const auto& stone : stones) {
+        std::cout << stone << " ";
+    }
+    std::cout << "\n";
+}
+
+long solve(std::list<long long>&& stones) {
+    const auto split = [](const long long num) -> std::pair<long long, long long> {
+        const auto str = std::to_string(num);
+        const auto half = str.begin() + str.size() / 2;
+
+        const std::string str1(str.begin(), half);
+        const std::string str2(half, str.end());
+        // std::cout << str1 << ", " << str2 << "\n";
+        return {std::stoll(str1), std::stoll(str2)};
+    };
+
+    for (int blink = 1; blink <= 25; blink++) {
+        std::cout << "Blink #" << blink << "\n";
+        for (auto it = stones.begin(); it != stones.end(); it++) {
+            if (*it == 0) {
+                *it = 1;
+                continue;
+            }
+
+            const long long num_digits = std::floor(std::log10(static_cast<double>(*it))) + 1;
+            if (num_digits > 0 and num_digits % 2 == 0) {
+                const auto&& [num1, num2] = split(*it);
+                *it = num1;
+                stones.insert(it, num2);
+                continue;
+            }
+
+            *it = (*it) * 2024;
+        }
+    }
+
+    return stones.size();
+}
+
+int main() {
+    static_assert(__cplusplus >= 201700L, "C++ version is not 17 or above");
+    std::fstream f("test.txt", std::ios::in);
+
+    if (!f.is_open()) {
+        std::cerr << "Failed to open the file." << std::endl;
+        return 1;
+    }
+
+    auto&& stones = read(f);
+    print(stones);
+    auto start = std::chrono::high_resolution_clock::now();
+    std::cout << solve(std::move(stones)) << "\n";
+    auto end = std::chrono::high_resolution_clock::now();
+    auto duration = std::chrono::duration_cast<std::chrono::microseconds>(end - start).count();
+    std::cout << "Elapsed time: " << duration << " μs\n";
+    return 0;
+}
